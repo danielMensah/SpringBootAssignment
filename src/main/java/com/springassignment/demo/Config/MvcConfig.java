dd31460b07935f2e4814dfb17ecfee947211dbb6
sergaben <gabense@hotmail.com>
2018-02-01 19:44:36 +0000

    solve configuration issues with thymeleaf

---
+++ src/main/java/com/springassignment/demo/Config/MvcConfig.java
@@ -0,0 +1,55 @@
+package com.springassignment.demo.Config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
+    }
+
+    @Bean
+    public TemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setEnableSpringELCompiler(true);
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
+
+    private ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("resources/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+
+        return templateResolver;
+    }
+}
--- src/main/java/com/springassignment/demo/Controller/SalaryController.java
+++ src/main/java/com/springassignment/demo/Controller/SalaryController.java
@@ -3,19 +3,24 @@ package com.springassignment.demo.Controller;
 import com.springassignment.demo.Model.EmployeeModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class SalaryController {
 
-    @RequestMapping("/calculateSalary")
-    public String showForm(Model model) {
+    @GetMapping("/calculatesalary")
+    public String salaryForm(Model model) {
         model.addAttribute("employee", new EmployeeModel());
-        return "employee";
+        return "calculatesalary";
     }
-
+    @PostMapping("/calculatesalary")
+    public String salarySubmit(@ModelAttribute EmployeeModel employee){
+        return "result";
+    }
     private float calculateMeanSalary(List<Float> listOfSalaries) {
         float mean = 0.0f;
 

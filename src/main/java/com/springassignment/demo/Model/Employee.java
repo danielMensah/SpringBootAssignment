3660f44ba66ef3842a53c1e25570be0b3c79251a
Aaron <aronners6@gmail.com>
2018-02-03 15:56:39 +0000

    aar-added-autogenerate-id-versiontwo

--- src/main/java/com/springassignment/demo/Model/Employee.java
+++ src/main/java/com/springassignment/demo/Model/Employee.java
@@ -1,6 +1,4 @@
 package com.springassignment.demo.Model;
-
-<<<<<<< HEAD
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,18 +8,7 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-=======
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-public class Employee {
-
-    @NotNull
->>>>>>> a3948912d8a7bf1a1aa627b5ba78ba0062b1d4e8
     private String name;
-
-    @NotNull
-    @Size(min=1000)
     private float salary;
 
     public Employee() {

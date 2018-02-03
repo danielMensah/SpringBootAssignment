7524e8bf0b8236a8680bfed65b0eda7c636853d2
sergaben <gabense@hotmail.com>
2018-02-03 16:27:21 +0000

    added a new class repository

---
+++ src/main/java/com/springassignment/demo/Database/EmployeeRespository.java
@@ -0,0 +1,19 @@
+package com.springassignment.demo.Database;
+
+import com.springassignment.demo.Model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeRespository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public Employee findById(int id){
+        return jdbcTemplate.queryForObject("select * from EMPLOYEE where id=?", new Object[]{
+                id
+        },new BeanPropertyRowMapper<Employee>(Employee.class));
+    }
+}

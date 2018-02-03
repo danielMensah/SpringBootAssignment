23eebf8bd781b24d80a2b4c3ddb422963bae5b89
danielMensah <mensahezros@gmail.com>
2018-02-03 12:57:16 +0000

    Now calculating the mean and returns employee with highest income

--- src/main/java/com/springassignment/demo/Controller/SalaryController.java
+++ src/main/java/com/springassignment/demo/Controller/SalaryController.java
@@ -1,29 +1,56 @@
 package com.springassignment.demo.Controller;
 
-import com.springassignment.demo.Model.EmployeeModel;
+import com.springassignment.demo.Model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Controller
 public class SalaryController {
+    private List<Employee> employeeList = new ArrayList<>();
 
-    @GetMapping("/calculatesalary")
+    @GetMapping("/salaryCalculator")
     public String salaryForm(Model model) {
-        model.addAttribute("employee", new EmployeeModel());
-        return "calculatesalary";
-    }
-    @PostMapping("/calculatesalary")
-    public String salarySubmit(@ModelAttribute EmployeeModel employee){
-        return "result";
-    }
-    private float calculateMeanSalary(List<Float> listOfSalaries) {
-        float mean = 0.0f;
 
-        return mean;
+        model.addAttribute("employeeList", employeeList);
+        model.addAttribute("employee", new Employee());
+        return "salaryCalculator";
+    }
+
+    @RequestMapping(value="/manageEmployee", method=RequestMethod.POST, params="action=add")
+    public String addEmployee(@ModelAttribute("employee") Employee employee){
+        employeeList.add(employee);
+        return "redirect:/salaryCalculator";
+    }
+
+    @RequestMapping(value="/manageEmployee", method=RequestMethod.POST, params="action=remove")
+    public String removeEmployee(@ModelAttribute("employee") Employee employee){
+        employeeList.remove(employee);
+        return "redirect:/salaryCalculator";
+    }
+
+    @RequestMapping(value="/manageEmployee", method=RequestMethod.POST, params="action=mean")
+    public String calculateMeanSalary(Model model) {
+        float sum = 0.0f;
+
+        for (Employee employee : employeeList) {
+            sum = sum + employee.getSalary();
+        }
+
+        float mean = sum / employeeList.size();
+        Employee employeeWithHighIncome = employeeList.stream().max(Comparator.comparing(Employee::getSalary)).get();
+
+        model.addAttribute("mean", mean);
+        model.addAttribute("employee", employeeWithHighIncome);
+
+        return "result";
     }
 }
---
+++ src/main/java/com/springassignment/demo/Model/Employee.java
@@ -0,0 +1,32 @@
+package com.springassignment.demo.Model;
+
+public class Employee {
+
+    private String name;
+    private float salary;
+
+    public Employee() {
+
+    }
+
+    public Employee(String name, float salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
